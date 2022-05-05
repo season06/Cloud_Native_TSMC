e446363d228be5b59ceac8641c41ee6722d4a7ba
season06 <mimi871006@gmail.com>
2022-05-05 19:26:16 +0800

    add: second test (red)

--- test_pottor.py
+++ test_pottor.py
@@ -9,3 +9,10 @@ def test_basics():
   assert 8 == get_price([3])
   assert 8 == get_price([4])
   assert 8 * 3 == get_price([1, 1, 1])
+
+
+def test_simple_discounts():
+  assert 8 * 2 * 0.95 == get_price([0, 1])
+  assert 8 * 3 * 0.9 == get_price([0, 2, 4])
+  assert 8 * 4 * 0.8 == get_price([0, 1, 2, 4])
+  assert 8 * 5 * 0.75 == get_price([0, 1, 2, 3, 4])
