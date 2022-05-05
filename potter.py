1ab61fa94fc20b2cebbb5657e3679a1122b1817e
season06 <mimi871006@gmail.com>
2022-05-05 19:37:02 +0800

    fix: second test (green)

--- potter.py
+++ potter.py
@@ -1,4 +1,10 @@
 PRICE = 8
+DISCOUNT = {
+    2: 0.95,
+    3: 0.9,
+    4: 0.8,
+    5: 0.75
+}
 
 def get_price(books):
-    return PRICE * len(books)
+    return PRICE * len(books) * DISCOUNT.get(len(set(books)), 1)
