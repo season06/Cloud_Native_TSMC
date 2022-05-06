0462d329bbb6e862b581955c68631638fbd2ed29
season06 <mimi871006@gmail.com>
2022-05-06 15:15:31 +0800

    fix: second refactor (green)

--- potter.py
+++ potter.py
@@ -1,28 +1,42 @@
-PRICE = 8
-DISCOUNT = {
-    1: 1,
-    2: 0.95,
-    3: 0.9,
-    4: 0.8,
-    5: 0.75
-}
+class Potter(object):
 
-def get_price(books):
-    total_price = 0
-    series_num = {i: 0 for i in range(1, 6)}
+    PRICE = 8
+    DISCOUNT = {
+        1: 1,
+        2: 0.95,
+        3: 0.9,
+        4: 0.8,
+        5: 0.75
+    }
 
-    while (books):
-        series = list(set(books))
-        series_num[len(series)] += 1
-        books = [i for i in books if not i in series or series.remove(i)]
+    def __init__(self, books):
+        self.books = books
+        self.total_price = 0
+        self.series_num = {i: 0 for i in range(1, 6)}
 
-    # (4+4) is cheaper than (3+5)
-    while (series_num.get(3, 0) and series_num.get(5, 0)):
-        series_num[3] -= 1
-        series_num[5] -= 1
-        series_num[4] += 2
-    
-    for length, num in series_num.items():
-        total_price += PRICE * length * num * DISCOUNT[length]
+    def __call__(self):
+        if len(self.books) == len(set(self.books)):
+            return self.PRICE * len(self.books) * self.DISCOUNT.get(len(self.books), 1)
 
-    return total_price
+        self.arrange_books()
+        self.check_cheaper()
+        self.calculate_price()
+
+        return self.total_price
+        
+    def arrange_books(self):
+        while (self.books):
+            series = list(set(self.books))
+            self.series_num[len(series)] += 1
+            self.books = [i for i in self.books if not i in series or series.remove(i)]
+
+    def check_cheaper(self): # (4+4) is cheaper than (3+5)
+        while (self.series_num.get(3, 0) and self.series_num.get(5, 0)):
+            self.series_num[3] -= 1
+            self.series_num[5] -= 1
+            self.series_num[4] += 2
+
+    def calculate_price(self):
+        for length, num in self.series_num.items():
+            self.total_price += self.PRICE * length * num * self.DISCOUNT[length]
+
--- test_pottor.py
+++ test_pottor.py
@@ -1,35 +1,35 @@
 import pytest
-from potter import get_price
+from potter import Potter
 
 
 def test_basics():
-  assert 0 == get_price([])
-  assert 8 == get_price([1])
-  assert 8 == get_price([2])
-  assert 8 == get_price([3])
-  assert 8 == get_price([4])
-  assert 8 * 3 == get_price([1, 1, 1])
+  assert 0 == Potter([])()
+  assert 8 == Potter([1])()
+  assert 8 == Potter([2])()
+  assert 8 == Potter([3])()
+  assert 8 == Potter([4])()
+  assert 8 * 3 == Potter([1, 1, 1])()
 
 
 def test_simple_discounts():
-  assert 8 * 2 * 0.95 == get_price([0, 1])
-  assert 8 * 3 * 0.9 == get_price([0, 2, 4])
-  assert 8 * 4 * 0.8 == get_price([0, 1, 2, 4])
-  assert 8 * 5 * 0.75 == get_price([0, 1, 2, 3, 4])
+  assert 8 * 2 * 0.95 == Potter([0, 1])()
+  assert 8 * 3 * 0.9 == Potter([0, 2, 4])()
+  assert 8 * 4 * 0.8 == Potter([0, 1, 2, 4])()
+  assert 8 * 5 * 0.75 == Potter([0, 1, 2, 3, 4])()
 
 
 def test_several_discounts():
-  assert 8 + (8 * 2 * 0.95) == get_price([0, 0, 1])
-  assert 2 * (8 * 2 * 0.95) == get_price([0, 0, 1, 1])
-  assert (8 * 4 * 0.8) + (8 * 2 * 0.95) == get_price([0, 0, 1, 2, 2, 3])
-  assert 8 + (8 * 5 * 0.75) == get_price([0, 1, 1, 2, 3, 4])
+  assert 8 + (8 * 2 * 0.95) == Potter([0, 0, 1])()
+  assert 2 * (8 * 2 * 0.95) == Potter([0, 0, 1, 1])()
+  assert (8 * 4 * 0.8) + (8 * 2 * 0.95) == Potter([0, 0, 1, 2, 2, 3])()
+  assert 8 + (8 * 5 * 0.75) == Potter([0, 1, 1, 2, 3, 4])()
 
 
 def test_edge_cases():
-  assert 2 * (8 * 4 * 0.8) == get_price([0, 0, 1, 1, 2, 2, 3, 4])
+  assert 2 * (8 * 4 * 0.8) == Potter([0, 0, 1, 1, 2, 2, 3, 4])()
   assert 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8) == \
-            get_price([0, 0, 0, 0, 0,
+            Potter([0, 0, 0, 0, 0,
                        1, 1, 1, 1, 1,
                        2, 2, 2, 2,
                        3, 3, 3, 3, 3,
-                       4, 4, 4, 4])
+                       4, 4, 4, 4])()
